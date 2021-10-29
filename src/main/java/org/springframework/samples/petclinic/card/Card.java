73090e75c532bddbacad27e3e9ab32fe08f083c0
EGM <ezegonmac01@gmail.com>
2021-10-30 00:03:51 +0200

    Added lombok @Data to Card

--- src/main/java/org/springframework/samples/petclinic/card/Card.java
+++ src/main/java/org/springframework/samples/petclinic/card/Card.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "cards")
 public class Card {
