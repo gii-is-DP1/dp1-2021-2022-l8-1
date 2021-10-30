996e22c836d0536d8c65dca6f13bbfe600166499
pabrivjim <parijimenez@gmail.com>
2021-10-30 23:22:05 +0200

    Entidad 'General' añadida con prueba unit y detalles de entidad 'Person' agregado

---
+++ src/main/java/org/springframework/samples/petclinic/general/GeneralRepository.java
@@ -0,0 +1,4 @@
+package org.springframework.samples.petclinic.general;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GeneralRepository extends CrudRepository<General, Integer>{}
