26ebb688a6ccc436ef631ae0be4bd401b2d5df75
EGM <ezegonmac01@gmail.com>
2021-10-31 14:17:05 +0100

    Created CardService

---
+++ src/main/java/org/springframework/samples/petclinic/card/CardService.java
@@ -0,0 +1,41 @@
+package org.springframework.samples.petclinic.card;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardService {
+    @Autowired
+    private CardRepository cardRepo;
+    
+    @Transactional
+    public int personCount() {
+        return (int) cardRepo.count();
+    }
+
+    @Transactional
+    public Iterable<Card> findAll() {
+        return cardRepo.findAll();
+    }
+
+    @Transactional
+    public Optional<Card> findPersonById(int id) {
+        return cardRepo.findById(id);
+    }
+
+    @Transactional
+    public void save(Card card){
+        cardRepo.save(card);
+    }
+
+    @Transactional
+    public void delete(Card card){
+        cardRepo.delete(card);
+    }
+
+
+}
