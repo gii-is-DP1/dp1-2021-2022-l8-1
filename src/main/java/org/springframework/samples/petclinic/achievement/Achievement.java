2c3faf23995466ff512df32e0809db3d4a7bf277
EGM <ezegonmac01@gmail.com>
2021-10-31 14:00:10 +0100

    Fixed test failure. Extended Card and Achievement with NamedEntity

--- src/main/java/org/springframework/samples/petclinic/achievement/Achievement.java
+++ src/main/java/org/springframework/samples/petclinic/achievement/Achievement.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name="achievements")
-public class Achievement {
+public class Achievement extends NamedEntity {
     
     @Column(name="name")
     @NotEmpty
--- src/main/java/org/springframework/samples/petclinic/card/Card.java
+++ src/main/java/org/springframework/samples/petclinic/card/Card.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "cards")
-public class Card {
+public class Card extends NamedEntity {
 
     @Column(name = "card_type")
     @NotEmpty
