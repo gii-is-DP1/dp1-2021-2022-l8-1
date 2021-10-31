1b857649e14674431f4cdb3bbe35789956593ca6
EGM <ezegonmac01@gmail.com>
2021-10-31 14:53:39 +0100

    Changed Card extends

--- src/main/java/org/springframework/samples/petclinic/card/Card.java
+++ src/main/java/org/springframework/samples/petclinic/card/Card.java
@@ -2,19 +2,22 @@ package org.springframework.samples.petclinic.card;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
-import org.springframework.samples.petclinic.model.NamedEntity;
+import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "cards")
-public class Card extends NamedEntity {
+public class Card extends BaseEntity {
 
     @Column(name = "card_type")
+    @Enumerated(EnumType.STRING)
     @NotEmpty
     private CARD_TYPE cardType;
 }
