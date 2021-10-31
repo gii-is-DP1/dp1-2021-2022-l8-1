eec07e98e1a1fccf0ce8dd558f3bd02e78f39ae1
Tomas Camero <tcamerob@gmail.com>
2021-10-31 15:34:58 +0100

    Creada entidad simple Cell

---
+++ src/main/java/org/springframework/samples/petclinic/cell/CellRepository.java
@@ -0,0 +1,4 @@
+package org.springframework.samples.petclinic.cell;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CellRepository extends CrudRepository<Cell, Integer>{}
