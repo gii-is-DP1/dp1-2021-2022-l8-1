261d7f232ccb83bc5d41a2dbf504108ece0c502b
EGM <ezegonmac01@gmail.com>
2021-11-06 17:28:41 +0100

    Revert "Added 2 new methods to PlayerRepository"
    This reverts commit 02615e88415bb3e0ed6527d86a920b5ba3ddf41c.

--- src/main/java/org/springframework/samples/petclinic/player/PlayerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/player/PlayerRepository.java
@@ -1,16 +1,4 @@
 package org.springframework.samples.petclinic.player;
 
-import java.util.List;
-
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.samples.petclinic.achievement.Achievement;
-
-public interface PlayerRepository extends CrudRepository<Player, Integer> {
-
-    //@Modifying
-    //void addAchievement(String name) throws DataAccessException;
-
-    //List<Achievement> findAchievements() throws DataAccessException;
-}
+public interface PlayerRepository extends CrudRepository<Player, Integer>{}
