8f54b36b731be65eca6b59e1aa022ef452e438a0
Tomas Camero <tcamerob@gmail.com>
2021-11-12 16:35:49 +0100

    Merge mal hecho, arreglo de error

--- src/main/java/org/springframework/samples/petclinic/card/Card.java
+++ src/main/java/org/springframework/samples/petclinic/card/Card.java
@@ -1,16 +1,19 @@
 package org.springframework.samples.petclinic.card;
 
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 
 import org.springframework.samples.petclinic.model.BaseEntity;
-
+import org.springframework.samples.petclinic.player.Player;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,7 +29,8 @@ public class Card extends BaseEntity {
     @NotEmpty
     private CARD_TYPE cardType;
 
-  
+    @ManyToMany(mappedBy = "cards")
+	private Set<Player> players;
 
     
 }
