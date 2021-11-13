18accf1fb1537bd6da521f38ccda86908e101c65
Ismael Pérez Ortiz <ismaelperezortiz@gmail.com>
2021-11-13 13:05:44 +0100

    Minor changes

--- src/test/java/org/springframework/samples/petclinic/game/GameServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/game/GameServiceTest.java
@@ -21,7 +21,7 @@ public class GameServiceTest {
     }
     
     @Test
-    public void testFindGamesByPlayerId(){
+    public void testFindGamesByPlayerId(){  // counts the games of a player
         Iterable<Game> games = gameService.findGamesByPlayerId(1);
         long count = games.spliterator().getExactSizeIfKnown();
         assertEquals(2, count);
