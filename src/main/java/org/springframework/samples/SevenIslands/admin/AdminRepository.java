97ebd92bdc7f1ce00e18cea0263b9ea0b0506350
migromarj <miguelromeroarjona@gmail.com>
2021-11-17 13:31:40 +0100

    Adding a check in games view. Now, when a player insert '/games', he only can see his games, but if the user is an admin, he can see all games

--- src/main/java/org/springframework/samples/SevenIslands/admin/AdminRepository.java
+++ src/main/java/org/springframework/samples/SevenIslands/admin/AdminRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface AdminRepository extends CrudRepository<Admin, Integer> {
     
+// @Query(value = "SELECT * FROM GAMES WHERE PLAYER_ID LIKE ?1", nativeQuery = true)
+// Collection<Admin> findAdminsById(@Param("playerId") int playerId) throws DataAccessException
     
 }
