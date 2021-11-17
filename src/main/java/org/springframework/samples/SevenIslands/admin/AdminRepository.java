fcb5d7d0383845e1e12f68c71b557c4f4b16e0de
Miguel Romero Arjona <80202474+migromarj@users.noreply.github.com>
2021-11-17 13:32:52 +0100

    Update AdminRepository.java

--- src/main/java/org/springframework/samples/SevenIslands/admin/AdminRepository.java
+++ src/main/java/org/springframework/samples/SevenIslands/admin/AdminRepository.java
@@ -4,7 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface AdminRepository extends CrudRepository<Admin, Integer> {
     
-// @Query(value = "SELECT * FROM GAMES WHERE PLAYER_ID LIKE ?1", nativeQuery = true)
-// Collection<Admin> findAdminsById(@Param("playerId") int playerId) throws DataAccessException
-    
 }
