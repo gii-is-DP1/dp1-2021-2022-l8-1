01f1914b679fdda534329287c4d71da3c58a85bf
pabrivjim <parijimenez@gmail.com>
2021-11-18 09:46:48 +0100

    board view added v1.0 (not showing the image yet due to unsolved problems)

--- src/main/java/org/springframework/samples/SevenIslands/board/Board.java
+++ src/main/java/org/springframework/samples/SevenIslands/board/Board.java
@@ -2,6 +2,7 @@ package org.springframework.samples.SevenIslands.board;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Positive;
 
 import org.springframework.samples.SevenIslands.model.BaseEntity;
 
@@ -13,5 +14,15 @@ import lombok.Setter;
 @Entity
 @Table(name="boards")
 public class Board extends BaseEntity{
-    
+    String background;
+    @Positive
+    int width;
+    @Positive
+    int height;
+
+    public Board(){
+        this.background="resources/images/board.png";
+        this.width=800;
+        this.height=800;
+    }
 }
