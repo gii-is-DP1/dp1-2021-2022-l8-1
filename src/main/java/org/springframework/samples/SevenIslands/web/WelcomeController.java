e6ac0fcb3567984580dd872165badc07eae4d760
pabrivjim <parijimenez@gmail.com>
2021-11-22 11:12:56 +0100

    boardController Map<> changed to ModelMap and some deprecated code

--- src/main/java/org/springframework/samples/SevenIslands/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/SevenIslands/web/WelcomeController.java
@@ -2,7 +2,6 @@ package org.springframework.samples.SevenIslands.web;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
