f28681ec81be6bca1c29f84ef089f0bfb2d216c1
migromarj <miguelromeroarjona@gmail.com>
2021-11-23 15:35:50 +0100

    Adding 6 tests for PlayerService methods

--- src/test/java/org/springframework/samples/SevenIslands/player/PlayerServiceTests.java
+++ src/test/java/org/springframework/samples/SevenIslands/player/PlayerServiceTests.java
@@ -2,6 +2,7 @@ package org.springframework.samples.SevenIslands.player;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.StreamSupport;
 
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.SevenIslands.user.AuthoritiesService;
+import org.springframework.samples.SevenIslands.user.User;
+import org.springframework.samples.SevenIslands.user.UserService;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
@@ -16,12 +20,72 @@ public class PlayerServiceTests {
 
     @Autowired
     private PlayerService playerService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthoritiesService authoritiesService;
     
     @Test
     public void testCountWithInitialData(){
         int count = playerService.playerCount();
         assertEquals(3, count);
     }
+
+    @Test
+    public void shouldInsertPlayerAndUser(){
+
+        Player player = new Player();
+        player.setFirstName("Antonio");
+        player.setSurname("García");
+        player.setEmail("antoniogar@gmail.com");
+        player.setProfilePhoto("www.foto.png");
+
+        User user = new User();
+        user.setUsername("antoniog11");
+        user.setPassword("4G4rc14");
+        user.setEnabled(true);
+
+        player.setUser(user);
+    
+        playerService.savePlayer(player);
+
+        assertThat(player.getId().longValue()).isNotEqualTo(0);
+
+
+    }
+
+    /*@Test
+    public void shouldDeletePlayer(){
+
+        Player player = new Player();
+        player.setFirstName("Antonio");
+        player.setSurname("García");
+        player.setEmail("antoniogar@gmail.com");
+        player.setProfilePhoto("www.foto.png");
+
+        User user = new User();
+        user.setUsername("antoniog11");
+        user.setPassword("4G4rc14");
+        user.setEnabled(true);
+
+        player.setUser(user);
+    
+        //Similar to .savePlayer(player)
+        playerService.save(player);
+        userService.saveUser(player.getUser());
+        authoritiesService.saveAuthorities(player.getUser().getUsername(), "player");
+
+        int countBefore = playerService.playerCount();
+        
+        playerService.delete(player);
+
+        int countAfter = playerService.playerCount();
+
+        assertNotEquals(countBefore, countAfter);
+
+    }*/
 
     @Test
     public void testFindPlayersByGameId() {
@@ -46,6 +110,36 @@ public class PlayerServiceTests {
     }
 
     @Test
+    public void testFinPlayerByUsername(){
+        int count = playerService.getPlayerByName("test2").size();
+        assertEquals(1,count);
+    }
+
+    @Test
+    public void testGetIdlayerByUsername(){
+        int playerId = playerService.getIdPlayerByName("test3");
+        assertEquals(3,playerId);
+    }
+
+    @Test
+    public void testFindPlayerBySurname(){
+        int count = playerService.findPlayerBySurname("Alonso").size();
+        assertEquals(1,count);
+    }
+
+    @Test
+    public void testGetAchievementsByPlayerId(){
+        long count = playerService.getAchievementsByPlayerId(1).spliterator().getExactSizeIfKnown();
+        assertEquals(2,count);
+    }
+
+    @Test
+    public void testGetCardsByPlayerId(){
+        long count = playerService.getCardsByPlayerId(1).spliterator().getExactSizeIfKnown();
+        assertEquals(3,count);
+    }
+
+    @Test
     public void testWatchGameByPlayerId(){
         Iterable<Player> players = playerService.findWatchGameByPlayerId(3);
         long count = players.spliterator().getExactSizeIfKnown();
@@ -58,7 +152,8 @@ public class PlayerServiceTests {
         assertEquals(1, players.spliterator().getExactSizeIfKnown());
     }
 
-    
+    //Historias de usuario
+
     @Test
     public void testCountAllPlayers() { //HISTORIA DE USUARIO H16 CASO DE USO POSITIVO
         long count = playerService.findAll().spliterator().getExactSizeIfKnown();
