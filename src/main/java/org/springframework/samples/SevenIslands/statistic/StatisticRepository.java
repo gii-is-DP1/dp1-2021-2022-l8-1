9632db58e10926bd25bcb29be30b9aacb8956888
EGM <ezegonmac01@gmail.com>
2021-12-05 17:45:01 +0100

    Commented old methods in StatisticRepo. & fixed news

--- src/main/java/org/springframework/samples/SevenIslands/statistic/StatisticRepository.java
+++ src/main/java/org/springframework/samples/SevenIslands/statistic/StatisticRepository.java
@@ -11,46 +11,46 @@ import org.springframework.samples.SevenIslands.player.Player;
 public interface StatisticRepository extends CrudRepository<Statistic, String>{
 
 
-    @Query("SELECT P FROM Player P ORDER BY P.totalWins DESC")
+    @Query("SELECT P FROM Player P ORDER BY COUNT(P.statistic.hadWon) DESC")
 	Collection<Player> findPlayersOrderedByWins() throws DataAccessException;
 
-	@Query("SELECT P FROM Player P ORDER BY P.totalPointsAllGames DESC")
+	@Query("SELECT P FROM Player P ORDER BY SUM(P.statistic.points) DESC")
 	Collection<Player> findPlayersOrderedByPoints() throws DataAccessException;
-
-
-    //  WINS (SUM, AVG)
-    @Query("SELECT SUM(S.wins) FROM Statistic S WHERE S.id = :playerId")
-    Integer findWinsCountByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
-
-    @Query("SELECT AVG(S.wins) FROM Statistic S WHERE S.id = :playerId")
-    Integer findWinsAvgByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
-
-
-    //  POINTS (SUM, AVG, MAX, MIN)
-    @Query("SELECT SUM(S.points) FROM Statistic S WHERE S.id = :playerId")
-    Integer findPointsByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
-
-    @Query("SELECT AVG(S.points) FROM Statistic S WHERE S.id = :playerId")
-    Double findAvgPointsByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
-
-    @Query("SELECT MAX(S.points) FROM Statistic S WHERE S.id = :playerId")
-    Integer findMaxPointsByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
     
-    @Query("SELECT MIN(S.points) FROM Statistic S WHERE S.id = :playerId")
-    Integer findMinPointsByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+
+    // //  WINS (SUM, AVG)
+    // @Query("SELECT SUM(S.wins) FROM Statistic S WHERE S.id = :playerId")
+    // Integer findWinsCountByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+
+    // @Query("SELECT AVG(S.wins) FROM Statistic S WHERE S.id = :playerId")
+    // Integer findWinsAvgByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+
+
+    // //  POINTS (SUM, AVG, MAX, MIN)
+    // @Query("SELECT SUM(S.points) FROM Statistic S WHERE S.id = :playerId")
+    // Integer findPointsByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+
+    // @Query("SELECT AVG(S.points) FROM Statistic S WHERE S.id = :playerId")
+    // Double findAvgPointsByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+
+    // @Query("SELECT MAX(S.points) FROM Statistic S WHERE S.id = :playerId")
+    // Integer findMaxPointsByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+    
+    // @Query("SELECT MIN(S.points) FROM Statistic S WHERE S.id = :playerId")
+    // Integer findMinPointsByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
   
 
-    //  GAME (COUNT, TIME-AVG, TIME-MAX, TIME-MIN)
-    @Query("SELECT COUNT(G) FROM Games G WHERE G.player.statistic.id = :playerId")
-    Integer findGamesCountByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+    // //  GAME (COUNT, TIME-AVG, TIME-MAX, TIME-MIN)
+    // @Query("SELECT COUNT(G) FROM Games G WHERE G.player.statistic.id = :playerId")
+    // Integer findGamesCountByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
 
-    @Query("SELECT AVG(G) FROM Games G WHERE G.player.statistic.id = playerId GROUP BY G.duration")
-    Double findAvgGameTimeByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+    // @Query("SELECT AVG(G) FROM Games G WHERE G.player.statistic.id = playerId GROUP BY G.duration")
+    // Double findAvgGameTimeByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
 
-    @Query("SELECT MAX(G) FROM Games G WHERE G.player.statistic.id = playerId GROUP BY G.duration")
-    Integer findMaxGameTimeByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+    // @Query("SELECT MAX(G) FROM Games G WHERE G.player.statistic.id = playerId GROUP BY G.duration")
+    // Integer findMaxGameTimeByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
     
-    @Query("SELECT MIN(G) FROM Games G WHERE G.player.statistic.id = playerId GROUP BY G.duration")
-    Integer findMinGameTimeByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
+    // @Query("SELECT MIN(G) FROM Games G WHERE G.player.statistic.id = playerId GROUP BY G.duration")
+    // Integer findMinGameTimeByPlayerId(@Param("playerId") int playerId) throws DataAccessException;
     
 }
