b1ef496787b44e7cb93d86f7517436304e468746
migromarj <miguelromeroarjona@gmail.com>
2021-12-09 11:33:04 +0100

    Adding some commented tests of Game controller

--- src/test/java/org/springframework/samples/SevenIslands/game/GameControllerTests.java
+++ src/test/java/org/springframework/samples/SevenIslands/game/GameControllerTests.java
@@ -17,9 +17,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.context.annotation.FilterType;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //@WebMvcTest(controllers = GameController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class GameControllerTests {
     
-    //private static final int TEST_GAME_ID = 10;
+    private static final int TEST_GAME_ID = 10;
 
     @Autowired
     private MockMvc mockMvc;
@@ -35,20 +38,18 @@ public class GameControllerTests {
     @MockBean
     private GameService gameService;
 
-    /*private Game firstGame;
-
     @BeforeEach
 	void setup() {
 
-		firstGame = new Game();
+		Game firstGame = new Game();
 		firstGame.setId(TEST_GAME_ID);
         firstGame.setName("First Game");
         firstGame.setCode("AHG28FD9J");
         firstGame.setPrivacity(PRIVACITY.PUBLIC);
-        gameService.save(firstGame);
-		given(this.gameService.findGameById(TEST_GAME_ID).get()).willReturn(firstGame);
+        //gameService.save(firstGame);
+		//given(this.gameService.findGameById(TEST_GAME_ID).get()).willReturn(firstGame);
 
-	}*/
+	}
 
     @WithMockUser(value = "spring")
 	@Test
@@ -57,5 +58,26 @@ public class GameControllerTests {
 				.andExpect(view().name("games/createOrUpdateGameForm"));
 	}
 
+	/*@WithMockUser(value = "spring")
+	@Test
+	void testJoinLobby() throws Exception {
+		mockMvc.perform(get("/{code}/lobby", "AHG28FD9J")).andExpect(status().isOk())
+				.andExpect(view().name("games/lobby"));
+	}*/
+
+	/*@WithMockUser(value = "spring")
+	@Test
+	void testExitGame() throws Exception {
+		mockMvc.perform(get("/exit/{gameId}", TEST_GAME_ID))
+				.andExpect(status().isOk())
+				.andExpect(view().name("games/publicRooms"));
+	}*/
+
+    @WithMockUser(value = "spring")
+	@Test
+	void testProcessCreationFormSuccess() throws Exception {
+		mockMvc.perform(post("/games/save").param("name", "My First Game").param("code", "ABCDF1234").param("privacity", "PUBLIC"))
+				.andExpect(status().is3xxRedirection());
+	}
 
 }
