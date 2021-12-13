425d091ff34ad940e3a65877f0e9c3199740b851
migromarj <miguelromeroarjona@gmail.com>
2021-12-13 17:40:25 +0100

    Adding some GameController and PlayerController tests

--- src/test/java/org/springframework/samples/SevenIslands/game/GameControllerTests.java
+++ src/test/java/org/springframework/samples/SevenIslands/game/GameControllerTests.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.SevenIslands.game;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.SevenIslands.admin.AdminController;
 import org.springframework.samples.SevenIslands.configuration.SecurityConfiguration;
 import org.springframework.samples.SevenIslands.deck.DeckService;
+import org.springframework.samples.SevenIslands.player.Player;
 import org.springframework.samples.SevenIslands.player.PlayerController;
 import org.springframework.samples.SevenIslands.player.PlayerService;
 import org.springframework.samples.SevenIslands.util.SecurityService;
@@ -17,16 +19,20 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.context.annotation.FilterType;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Optional;
 
 
 @WebMvcTest(controllers = GameController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class GameControllerTests {
     
     private static final int TEST_GAME_ID = 10;
+    private static final int TEST_PLAYER_ID = 10;
 
 	@Autowired
 	private GameController gameController;
@@ -53,15 +59,28 @@ public class GameControllerTests {
     private DeckService deckService;
 
 	private Game firstGame;
+    private Player firstPlayer;
 
     @BeforeEach
 	void setup() {
+
+        firstPlayer = new Player();
+        firstPlayer.setId(TEST_PLAYER_ID);
+        firstPlayer.setProfilePhoto("https://imagen.png");
+        firstPlayer.setFirstName("Manuel");
+        firstPlayer.setSurname("González");
+        firstPlayer.setEmail("manuelgonzalez@gmail.com");
 
 		firstGame = new Game();
 		firstGame.setId(TEST_GAME_ID);
         firstGame.setName("First Game");
         firstGame.setCode("AHG28FD9J");
         firstGame.setPrivacity(PRIVACITY.PUBLIC);
+        firstGame.setHas_started(true);
+        firstGame.setPlayer(firstPlayer);
+
+        when(this.gameService.findGameById(TEST_GAME_ID)).thenReturn(Optional.of(firstGame));
+        when(this.securityService.getCurrentPlayerId()).thenReturn(TEST_PLAYER_ID);
 	}
 
     @WithMockUser(value = "spring")
@@ -70,5 +89,15 @@ public class GameControllerTests {
 		mockMvc.perform(get("/games/new")).andExpect(status().isOk()).andExpect(model().attributeExists("game"))
 				.andExpect(view().name("games/createOrUpdateGameForm"));
 	}
+
+    @Disabled
+    @WithMockUser(value = "spring")
+	@Test
+	void testExitGame() throws Exception {
+		mockMvc.perform(get("/games/exit/{gameId}",TEST_GAME_ID)).andExpect(status().is3xxRedirection())
+				.andExpect(view().name("/welcome"));
+	}
+
+    
 
 }
--- src/test/java/org/springframework/samples/SevenIslands/player/PlayerControllerTests.java
+++ src/test/java/org/springframework/samples/SevenIslands/player/PlayerControllerTests.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.SevenIslands.player;
 
 import org.junit.jupiter.api.Test;
+import org.hamcrest.beans.HasProperty;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,19 +12,26 @@ import org.springframework.samples.SevenIslands.achievement.AchievementService;
 import org.springframework.samples.SevenIslands.configuration.SecurityConfiguration;
 import org.springframework.samples.SevenIslands.game.GameService;
 import org.springframework.samples.SevenIslands.general.GeneralService;
+import org.springframework.samples.SevenIslands.user.Authorities;
 import org.springframework.samples.SevenIslands.user.AuthoritiesService;
+import org.springframework.samples.SevenIslands.user.User;
 import org.springframework.samples.SevenIslands.user.UserService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.context.annotation.FilterType;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.Optional;
+import java.util.Set;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
@@ -41,13 +50,13 @@ public class PlayerControllerTests {
     private PlayerService playerService;
 
     @MockBean	
-	  private GeneralService generalService;
+	private GeneralService generalService;
 
     @MockBean
     private GameService gameService;
 
     @MockBean	
-	  private UserService userService;
+	private UserService userService;
     
     @MockBean
     private AuthoritiesService authoritiesService;
@@ -55,21 +64,69 @@ public class PlayerControllerTests {
     @MockBean
     private AchievementService achievementService;
 
-
-
-
     private Player otherPlayer;
+    private User otherUser;
+    private Authorities auth;
 
     @BeforeEach
 	void setup() {
 
 		otherPlayer = new Player();
         otherPlayer.setId(TEST_PLAYER_ID);
+        otherPlayer.setProfilePhoto("https://imagen.png");
         otherPlayer.setFirstName("Manuel");
         otherPlayer.setSurname("González");
         otherPlayer.setEmail("manuelgonzalez@gmail.com");
+        
+        otherUser = new User();
+        otherUser.setUsername("manugo44");
+        otherUser.setPassword("ManueGonza9!");
+        otherUser.setEnabled(true);
+
+        auth = new Authorities();
+        auth.setAuthority("player");
+        Set<Authorities> st = Set.of(auth);
+
+        otherUser.setAuthorities(st);
+        otherPlayer.setUser(otherUser);
 
         when(this.playerService.findPlayerById(TEST_PLAYER_ID)).thenReturn(Optional.of(otherPlayer));
+	}
+
+    @Disabled
+    @WithMockUser(value = "spring")
+	@Test
+	void testListPlayers() throws Exception {
+		mockMvc.perform(get("/players")).andExpect(status().isOk())
+				.andExpect(view().name("players/listPlayers"));
+	}
+
+    @WithMockUser(value = "spring")
+	@Test
+	void testPlayerProfile() throws Exception {
+		mockMvc.perform(get("/players/profile/{playerId}",TEST_PLAYER_ID))
+                .andExpect(status().isOk()).andExpect(model().attributeExists("player"))
+				.andExpect(view().name("/players/profile"));
+	}
+
+    @WithMockUser(value = "spring")
+	@Test
+	void testPlayerProfileAchievements() throws Exception {
+		mockMvc.perform(get("/players/profile/{playerId}/achievements",TEST_PLAYER_ID))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("player"))
+                .andExpect(model().attributeExists("achieved"))
+                .andExpect(model().attributeExists("notAchieved"))
+				.andExpect(view().name("players/achievements"));
+	}
+
+    @WithMockUser(value = "spring")
+	@Test
+	void testPlayerProfileStatistics() throws Exception {
+		mockMvc.perform(get("/players/profile/{playerId}/statistics",TEST_PLAYER_ID))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("player"))
+				.andExpect(view().name("players/statistics"));
 	}
 
     @WithMockUser(value = "spring")
@@ -81,10 +138,39 @@ public class PlayerControllerTests {
 
     @WithMockUser(value = "spring")
 	@Test
+	void testRoomsPlayer() throws Exception {
+		mockMvc.perform(get("/players/rooms"))
+				.andExpect(status().isOk()).andExpect(model().attributeExists("games"))
+				.andExpect(view().name("games/publicRooms"));
+	}
+
+    @WithMockUser(value = "spring")
+	@Test
 	void testProfilePlayer() throws Exception {
 		mockMvc.perform(get("/players/profile/{playerId}/rooms/played", TEST_PLAYER_ID))
 				.andExpect(status().isOk()).andExpect(model().attributeExists("player"))
 				.andExpect(view().name("players/roomsPlayed"));
+	}
+
+    @WithMockUser(value = "spring")
+	@Test
+	void testProfile2Player() throws Exception {
+		mockMvc.perform(get("/players/profile/{playerId}/rooms/created", TEST_PLAYER_ID))
+				.andExpect(view().name("players/roomsCreated"));
+	}
+
+    @Disabled
+    @WithMockUser(value = "spring")
+	@Test
+	void testProcessUpdatePlayerFormSuccess() throws Exception {
+		mockMvc.perform(post("/players/edit/{playerId}", TEST_PLAYER_ID)
+        .param("profilePhoto", "https://imagen.png")
+        .param("firstName","Manuel")
+        .param("surname","González")
+        .param("email","manuelgonzalez@gmail.com"))
+        //.andExpect(status().is3xxRedirection())
+        .andExpect(model().attributeExists("player"))
+	    .andExpect(view().name("/players/listPlayers"));
 	}
 
 }
