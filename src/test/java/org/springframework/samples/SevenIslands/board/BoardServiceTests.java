89fd6b76235180828540a8874de8b9ef1f658ddd
pabrivjim <parijimenez@gmail.com>
2022-01-19 00:09:40 +0100

    initCard test

--- src/test/java/org/springframework/samples/SevenIslands/board/BoardServiceTests.java
+++ src/test/java/org/springframework/samples/SevenIslands/board/BoardServiceTests.java
@@ -3,14 +3,20 @@ package org.springframework.samples.SevenIslands.board;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.SevenIslands.card.CARD_TYPE;
+import org.springframework.samples.SevenIslands.card.Card;
+import org.springframework.samples.SevenIslands.deck.Deck;
+import org.springframework.samples.SevenIslands.deck.DeckService;
+import org.springframework.samples.SevenIslands.game.Game;
 import org.springframework.samples.SevenIslands.game.GameService;
 import org.springframework.samples.SevenIslands.player.Player;
 import org.springframework.samples.SevenIslands.player.PlayerService;
@@ -25,6 +31,8 @@ public class BoardServiceTests {
     private GameService gameService;
     @Autowired
     private PlayerService playerService;
+    @Autowired
+    private DeckService deckService;
 
     @Test
     public void testCountWithInitialData(){
@@ -47,11 +55,37 @@ public class BoardServiceTests {
         assertEquals(board.getHeight(),644);
     
     }
+
+    @Test
+    public void testInitCardPlayers(){ 
+        String code = "ABCD122";
+        Game game = gameService.findGamesByRoomCode(code).iterator().next();
+        Integer nPlayers = (int) game.getPlayers().stream().count();
+        Deck d = deckService.init("ABCD122");
+        int initSize = game.getDeck().getCards().size();
+
+        for(Player p: game.getPlayers()){
+            List<Card> cards = new ArrayList<>();      
+            List<Card> doblones = d.getCards().stream().filter(x->x.getCardType().equals(CARD_TYPE.DOUBLON)).limit(3).collect(Collectors.toList());  
+            d.getCards().removeAll(doblones);
+            cards.addAll(doblones);
+            assertEquals(3, doblones.size());
+            assertEquals(3, cards.size());
+        }
+
+        asserEquals(initSize, d.getCards().size()-nPlayers*3);
+        
+
+    
+    }
+
+    private void asserEquals(int initSize, int i) {
+    }
+
     @Test
     public void testLeaveGame(){ 
         String code = "ABCD124";
         List<Player> players = gameService.findGamesByRoomCode(code).iterator().next().getPlayers();
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + players.stream().map(x-> x.getUser()));
         Player player1 = playerService.findPlayerById(1).get();
         if(players.stream().anyMatch(x->x.equals(player1))){
             String none = boardService.leaveGame(player1, code);
